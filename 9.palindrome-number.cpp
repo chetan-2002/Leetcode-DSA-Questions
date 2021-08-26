1cb629ac6002ec58952f83d2b8ff42c16c66274d
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2021-08-26 18:50:01 +0530

    files changed

--- 9.palindrome-number.cpp
+++ 9.palindrome-number.cpp
@@ -8,7 +8,18 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        
+        long long int rev=0;
+        int temp=x;
+        while(x>0){
+            int last=x%10;
+            rev=rev*10 +last;
+            x/=10;
+        }
+        if(rev==temp){
+            return true;
+        }else{
+            return false;
+        }
     }
 };
 // @lc code=end
