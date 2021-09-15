173fa50619b2c8892418ceeb34ad83f7b3c403a2
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2021-09-15 10:52:18 +0530

    new files added

---
+++ 1720.decode-xo-red-array.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1720 lang=cpp
+ *
+ * [1720] Decode XORed Array
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    vector<int> decode(vector<int> &encoded, int first)
+    {
+        vector<int>v(encoded.size() + 1);
+        v[0] = first;
+        for (int i = 0; i < encoded.size(); i++)
+        {
+            v[i + 1] = v[i] ^ encoded[i];
+        }
+        return v;
+    }
+};
+// @lc code=end
