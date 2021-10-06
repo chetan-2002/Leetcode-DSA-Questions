04ea3d0962a99600a30967da729595d90bb3bbd4
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2021-10-06 19:50:51 +0530

    new files added

--- 94.binary-tree-inorder-traversal.cpp
+++ 94.binary-tree-inorder-traversal.cpp
@@ -19,24 +19,31 @@
 class Solution
 {
 public:
-    vector<int> ans;
-    void inorder(TreeNode *root, vector<int> &ans)
-    {
-        if (root == NULL)
-        {
-            return;
-        }
-        inorder(root->left, ans);
-        ans.push_back(root->val);
-        inorder(root->right, ans);
-    }
     vector<int> inorderTraversal(TreeNode *root)
     {
-        if (root == NULL)
+        vector<int> ans;
+        stack<TreeNode *> st;
+
+        TreeNode *node = root;
+        while (true)
         {
-            return ans;
+            if (node != NULL)
+            {
+                st.push(node);
+                node = node->left;
+            }
+            else
+            {
+                if (st.empty() == true)
+                {
+                    break;
+                }
+                node = st.top();
+                st.pop();
+                ans.push_back(node->val);
+                node = node->right;
+            }
         }
-        inorder(root, ans);
         return ans;
     }
 };
