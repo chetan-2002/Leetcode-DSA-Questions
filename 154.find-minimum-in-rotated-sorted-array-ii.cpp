aa814c90ff6299048c8988e1f186c2b42f5993f9
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2021-10-23 14:42:22 +0530

    new files added

---
+++ 154.find-minimum-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=154 lang=cpp
+ *
+ * [154] Find Minimum in Rotated Sorted Array II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        return *min_element(nums.begin(), nums.end());
+    }
+};
+// @lc code=end
+
