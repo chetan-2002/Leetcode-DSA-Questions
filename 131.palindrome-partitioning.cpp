9ddd403f44c88ba82f07a1fa752276af8f4fcedf
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-01-05 19:26:16 +0530

    new files added

---
+++ 131.palindrome-partitioning.cpp
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=131 lang=cpp
+ *
+ * [131] Palindrome Partitioning
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    bool isPalindrome(string s, int start, int end)
+    {
+        while (start <= end)
+        {
+            if (s[start++] != s[end--])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    void func(int index, string s, vector<string> &path, vector < vector<string>> & res)
+    {
+        if (index == s.size())
+        {
+            res.push_back(path);
+            return;
+        }
+        for (int i = index; i < s.size(); i++)
+        {
+            if (isPalindrome(s, index, i))
+            {
+                path.push_back(s.substr(index, i - index + 1));
+                func(i + 1, s, path, res);
+                path.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s)
+    {
+        vector<vector<string>> res;
+        vector<string> path;
+        func(0, s, path, res);
+        return res;
+    }
+};
+// @lc code=end
