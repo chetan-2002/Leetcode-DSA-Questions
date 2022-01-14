750b99fc042cd16294e836929b4943ad595ae706
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-01-14 18:33:56 +0530

    new files added

---
+++ 8.string-to-integer-atoi.cpp
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=8 lang=cpp
+ *
+ * [8] String to Integer (atoi)
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int myAtoi(string s)
+    {
+        int n = s.length(), i = 0;
+        bool flag = true;
+
+        while (i < n and s[i] == ' ')
+            i++;
+
+        if (s[i] == '+')
+            i++;
+        else if (s[i] == '-')
+        {
+            flag = false;
+            i++;
+        }
+
+        long res = 0;
+        while (i < n)
+        {
+            if (s[i] >= '0' and s[i] <= '9')
+            {
+                res = res * 10 + (s[i] - '0');
+                if (res >= INT_MAX)
+                    break;
+            }
+            else if ((s[i] == '+' || s[i] == '-') && res == 0)
+                return 0;
+            else
+                break;
+            i++;
+        }
+
+        if (!flag)
+            res *= -1;
+
+        if (res <= INT_MIN)
+            return INT_MIN;
+        else if (res >= INT_MAX)
+            return INT_MAX;
+        return res;
+    }
+};
+// @lc code=end
