34b14c39a485e02eaa0bc7ee3fc1a319fb5f5ec8
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-02-23 18:48:41 +0530

    new files added

---
+++ 133.clone-graph.cpp
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=133 lang=cpp
+ *
+ * [133] Clone Graph
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution
+{
+public:
+    Node *clone(Node *node, unordered_map<int, Node *> &visited)
+    {
+        Node *new_node = new Node(node->val);
+        visited.insert({node->val, new_node});
+        for (Node *n : node->neighbors)
+        {
+            auto it = visited.find(n->val);
+            if (it == visited.end())
+            {
+                Node *cn = clone(n, visited);
+                new_node->neighbors.push_back(cn);
+            }
+            else
+            {
+                new_node->neighbors.push_back(it->second);
+            }
+        }
+        return new_node;
+    }
+    Node *cloneGraph(Node *node)
+    {
+        if (node == NULL)
+        {
+            return node;
+        }
+        unordered_map<int, Node *> visited;
+        return clone(node, visited);
+    }
+};
+// @lc code=end
