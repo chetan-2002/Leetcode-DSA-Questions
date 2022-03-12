a9c1323573a99358ffa09b92ebe5a3769889a2e4
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-03-12 23:37:00 +0530

    new files added

---
+++ 138.copy-list-with-random-pointer.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode id=138 lang=cpp
+ *
+ * [138] Copy List with Random Pointer
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution
+{
+public:
+    Node *copyRandomList(Node *head)
+    {
+        unordered_map<Node *, Node *> mp;
+        if (head == NULL)
+            return NULL;
+        Node *temp = head;
+        while (temp)
+        {
+            mp[temp] = new Node(temp->val);
+            temp = temp->next;
+        }
+        temp = head;
+        while (temp)
+        {
+            mp[temp]->next = mp[temp->next];
+            mp[temp]->random = mp[temp->random];
+            temp = temp->next;
+        }
+        return mp[head];
+    }
+};
+// @lc code=end
